cbb7f713517643352df187d2e04c63e0e3987305
Nicolas <30669856+GiveWife@users.noreply.github.com>
2024-02-21 11:22:08 +0100

    package structure

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    /**
+     * A beautiful start for a beautiful project
+     * @param args
+     */
     public static void main(String[] args) {
-        
+
     }
+
 }
