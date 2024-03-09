6ddb8e5d8af8d2ce4f78b1c81c71cfa2c7dbed4a
KwintenCo <91952498+KwintenCo@users.noreply.github.com>
2024-03-09 09:49:54 +0100

    Use Case UML: Inspect Buffer Contents
    Algemene lijnen: pas als render() geïmplementeerd is, kan dit verder afgemaakt worden

--- tests/core/SaveBufferTest.java
+++ tests/core/SaveBufferTest.java
@@ -1,4 +1,17 @@
 package core;
 
+import org.junit.jupiter.api.Test;
+
 public class SaveBufferTest {
+    /*
+    Test moving the focus (4.4.1) and showing the focused view as having focus (4.4.2à
+     */
+    @Test
+    public void testMoveFocus(){
+        String[] paths = new String[]{"testresources/test.txt","testresources/test2.txt"};
+        Controller testController = new Controller(paths);
+        
+
+
+    }
 }
