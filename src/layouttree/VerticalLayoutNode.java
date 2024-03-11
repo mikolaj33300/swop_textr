defec6eab2a57dd460f7b1db72f6938ce407e5e6
KwintenCo <91952498+KwintenCo@users.noreply.github.com>
2024-03-11 20:06:55 +0100

    Aanvullingen en Fixes voor LayoutNode functies

--- src/layouttree/VerticalLayoutNode.java
+++ src/layouttree/VerticalLayoutNode.java
@@ -5,27 +5,40 @@ import java.util.Arrays;
 
 public class VerticalLayoutNode extends LayoutNode{
 
+    /**
+     * Constructor for VerticalLayoutNode, clones its arguments to prevent representation exposure
+     */
+    public VerticalLayoutNode(ArrayList<Layout> newChildren) {
+        super(newChildren);
+    }
+
+    /**
+     * Returns the orientation of the VerticalLayoutNode
+     */
     @Override
     public Orientation getOrientation() {
         return Orientation.VERTICAL;
     }
 
-    public VerticalLayoutNode(ArrayList<Layout> newChildren) {
-        super(newChildren);
-    }
-
+    /**
+     * Rotates the VerticalLayoutNode's child and its neighbour on the right
+     */
     @Override
     protected LayoutNode getNewMergedRotatedChild(ROT_DIRECTION rotdir, Layout child) {
         LayoutLeaf newSibling = this.getRightNeighbor(child);
-        ArrayList<Layout> nextChildren;
+        ArrayList<Layout> newChildren;
         if(rotdir == ROT_DIRECTION.CLOCKWISE){
-            nextChildren = new ArrayList<>(Arrays.asList(newSibling, child));
+            newChildren = new ArrayList<>(Arrays.asList(newSibling, child));
         } else {
-            nextChildren = new ArrayList<>(Arrays.asList(child, newSibling));
+            newChildren = new ArrayList<>(Arrays.asList(child, newSibling));
         }
-        return new HorizontalLayoutNode(children);
+        return new HorizontalLayoutNode(newChildren);
     }
 
+    /**
+     * Checks a given LayoutNode can be a child of this VerticalLayoutNode
+     * A child should have a different orientation than its parent
+     */
     @Override
     protected boolean isAllowedToBeChildOf(LayoutNode layoutNode) {
         if(parent.getOrientation()==Orientation.VERTICAL){
@@ -35,8 +48,16 @@ public class VerticalLayoutNode extends LayoutNode{
         }
     }
 
+    /**
+     Makes a deepcopy of VerticalLayoutNode
+     The references to this object and its contents will be removed
+     */
     @Override
     protected VerticalLayoutNode clone() {
-        return null;
+        ArrayList<Layout> deepCopyList = new ArrayList<>();
+        for(Layout l : super.children){
+            deepCopyList.add(l.clone());
+        }
+        return new VerticalLayoutNode(deepCopyList);
     }
 }
