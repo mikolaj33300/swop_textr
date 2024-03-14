40ecab9e01339fcc026e3eba9e396390795dbf84
Nicolas <30669856+GiveWife@users.noreply.github.com>
2024-03-14 23:58:31 +0100

    tests

--- tests/core/InspectBufferTest.java
+++ tests/core/InspectBufferTest.java
@@ -3,6 +3,14 @@ package core;
 import org.junit.jupiter.api.*;
 
 public class InspectBufferTest {
+
+    Controller c;
+    private final String root = "testresources/";
+    private final String path1 = root + "test.txt";
+    private final String path2 = root + "test2.txt";
+    private final String path3 = root + "test3.txt";
+
+
     @Test
     public void testInspectBuffer(){
         Controller c1 = new Controller(new String[]{"testresources/test.txt", "testresources/test2.txt"});
