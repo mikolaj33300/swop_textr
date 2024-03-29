bc887ff0ded4f8e29edbd64c93f7c4ce6265c9eb
spykyvenator <spykyvenator@protonmail.com>
2024-03-29 15:21:24 +0100

    edit: correct throw in facade constructor

--- src/controller/controllerFacade.java
+++ src/controller/controllerFacade.java
@@ -27,7 +27,7 @@ class ControllerFacade {
                 this.activeUseCaseController = new FileErrorPopupController(this);
             else
                 try {
-                    leaves.add(new LayoutLeaf(args[i], i == 0));
+                    leaves.add(new LayoutLeaf(path, false, ));
                 } catch (IOException e){
                     activeUseCaseController = new FileErrorPopupController(this);
                 }
@@ -44,11 +44,25 @@ class ControllerFacade {
      * Creates a ControllerFacade object.
      * Creates a {@link Layout} object which represents the root layout.
      * its children {@link LayoutLeaf} will be assigned according to arguments given by {@link TextR#main(String[])}
+     * @throws IOException when the path is invalid
      */
-	public void ControllerFacade(String[] paths){
-		this.rootLayout = initRootLayout(args);
-		for (String path:paths) {
-			this.windows.add(new window(new view(path), new inputhandler(path)));
+	public void ControllerFacade(String[] paths) throws IOException {
+		ArrayList<Layout> leaves = new ArrayList<LayoutLeaf>(paths.length);
+
+		for (int i = 0; i < paths.length; i++) {
+		    Path checkPath = Paths.get(paths[i]); 
+		    if (!Files.exists(checkPath)) 
+			    this.activeUseCaseController = new FileErrorPopupController(this);
+		    else
+			    leaves.add(new LayoutLeaf(path, false, ));
+
+		    this.windows.add(new window(new view(paths[i]), new inputhandler(paths[i])));
+		    leaves.add(new LayoutLeaf(paths[i], i==0, windows[i].view.getHash()));
 		}
+
+		if(leaves.size() == 1)
+		    this.rootLayout = leaves.get(0);
+		else
+		    this.rootLayout = new VerticalLayoutNode(leaves);
 	}
 }
