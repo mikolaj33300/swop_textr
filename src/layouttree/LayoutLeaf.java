9422ad751cf0de331ed08cde6e3ac1f2251b6374
KwintenCo <91952498+KwintenCo@users.noreply.github.com>
2024-04-16 22:35:50 +0200

    Documentatie Layout en LayoutLeaf in orde gebracht + Alle Functies herordend voor een logischere schikking

--- src/layouttree/LayoutLeaf.java
+++ src/layouttree/LayoutLeaf.java
@@ -14,34 +14,34 @@ import java.util.HashMap;
 public class LayoutLeaf extends Layout {
 
     /**
-     * Then hashCode that links this LayoutLeaf to the ControllerFacade
+     * The hashcode that links this LayoutLeaf to the ControllerFacade
      */
     private int containedHashCode;
 
     /**
      * Constructor for LayoutLeaf
-     * Takes an integer hashCode as input
+     * Sets the containedHashCode to the given hashCode
      */
     public LayoutLeaf(int hashCode) {
-	    this.containedHashCode = hashCode;
+        this.containedHashCode = hashCode;
     }
 
 
     /**
-     * Returns the hashCode contained in this LayoutLeaf
+     * Returns the containedHashCode contained in this LayoutLeaf
      */
     public int getContainedHashCode(){
         return  this.containedHashCode;
     }
 
     /**
-     * Deletes this LayoutLeaf from its parent list of children if it has a parent
+     * If the containHashCode of this LayoutLeaf is equal to the target-hash,
+     * Its containedHashCode will be changed to newHashCode
      */
     @Override
-    protected void deleteLeftLeaf() {
-        if (super.parent != null) {
-            super.parent.delete(this);
-        }
+    public void changeHash(int target, int newHashCode) {
+        if(this.containedHashCode == target)
+            this.containedHashCode = newHashCode;
     }
 
     /**
@@ -53,21 +53,6 @@ public class LayoutLeaf extends Layout {
             return this.getRightNeighborsContainedHash(hashCode);
         } else {
             return this.getLeftNeighborsContainedHash(hashCode);
-        }
-    }
-
-    /**
-     * Returns the hashCode of the neighbor to the right of this LayoutLeaf in the layouttree
-     */
-    private int getRightNeighborsContainedHash(int hash) throws HashNotMatchingException{
-        if(this.containedHashCode == hash){
-            if(parent != null){
-                return parent.getRightNeighbourContainedHash(this);
-            } else {
-                return this.containedHashCode;
-            }
-        } else {
-            throw new HashNotMatchingException();
         }
     }
 
@@ -87,49 +72,60 @@ public class LayoutLeaf extends Layout {
     }
 
     /**
-     * Rotates the layoutLeaf under this structure if there is one with its right neighbor if there is one,
-     * clockwise or counterclockwise according to rotdir. If the right neighbor is a direct sibling it rotates to stand
-     * perpendicular to the current orientation of the parent. If there is no direct right sibling it is added to the parent of the
-     * active leaf and rotated then.
+     * Returns the hashCode of the neighbor to the right of this LayoutLeaf in the layouttree
      */
-    public Layout rotateRelationshipNeighbor(ROT_DIRECTION rot_dir, int hash) {
-        if (parent != null && hash == this.containedHashCode) {
-            return parent.rotateWithRightSibling(rot_dir, this);
-        } else if (hash == this.containedHashCode) {
-            System.out.print((char) 7); //sounds terminal bell
-            return this;
+    private int getRightNeighborsContainedHash(int hash) throws HashNotMatchingException{
+        if(this.containedHashCode == hash){
+            if(parent != null){
+                return parent.getRightNeighbourContainedHash(this);
+            } else {
+                return this.containedHashCode;
+            }
         } else {
             throw new HashNotMatchingException();
         }
     }
 
+    /**
+     * Returns the containedHashCode of this LayoutLeaf
+     */
+    @Override
+    protected int getLeftmostContainedHash() {
+        return containedHashCode;
+    }
+
+    /**
+     * Returns the containedHashCode of this LayoutLeaf
+     */
+    @Override
+    protected int getRightmostContainedHash() {
+        return containedHashCode;
+    }
+
+    /**
+     * Returns this LayoutLeaf
+     */
     @Override
     protected LayoutLeaf getLeftLeaf() {
         return this;
     }
 
     /**
-     * Determines if the given LayoutLeaf and Object are equal content-wise (not reference-wise)
-     * Determines if the given layout is a {@link LayoutLeaf} and their {@link FileBuffer}'s are also equal
+     * Returns HashMap with an entry of this LayoutLeafs containedHashCode, linked to the given uiCoordsScaled
+     * This represents the part of the terminal that this LayoutLeaf will fill up
      */
     @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof LayoutLeaf leaf) {
-            return (this.containedHashCode == leaf.containedHashCode);
-        } else {
-            return false;
-        }
+    public HashMap<Integer, Rectangle> getCoordsList(Rectangle uiCoordsScaled) {
+        HashMap<Integer, Rectangle> mapToReturn = new HashMap<Integer, Rectangle>();
+        mapToReturn.put(this.containedHashCode, uiCoordsScaled);
+        return mapToReturn;
     }
+
 
     /**
-     * Checks if a this LayoutLeaf is allowed to be a child of the given LayoutNode
-     * Two children of a LayoutNode are not allowed to be active at the same time
+     * If this LayoutLeafs containedHashCode equals hashToAdd, this function returns the parent of this LayoutLeaf, with
+     * a new LayoutLeaf, with containedHashCode equal to hashToAdd, inserted right next to this LayoutLeaf,
      */
-    @Override
-    protected boolean isAllowedToBeChildOf(LayoutNode futureParent) {
-        return true;
-    }
-
     @Override
     public Layout insertRightOfSpecified(int hashSpecified, int hashToAdd) {
         //TODO: Check if hashToAdd already exists
@@ -148,6 +144,36 @@ public class LayoutLeaf extends Layout {
         }
     }
 
+    /**
+     * Rotates this LayoutLeaf if its containedHashCode equal to the given targetHashcode in the layouttree of its parent
+     * (if it has one) with its right neighbor if it exists; if it doesn't, a bell-sound will be sounded
+     * The rotation happens clockwise or counterclockwise according to rotdir.
+     */
+    public Layout rotateRelationshipNeighbor(ROT_DIRECTION rot_dir, int hash) {
+        if (parent != null && hash == this.containedHashCode) {
+            return parent.rotateWithRightSibling(rot_dir, this);
+        } else if (hash == this.containedHashCode) {
+            System.out.print((char) 7); //sounds terminal bell
+            return this;
+        } else {
+            throw new HashNotMatchingException();
+        }
+    }
+
+
+    /**
+     * Returns true
+     * This is the case because a LayoutLeaf is always allowed to be a child of a LayoutNode
+     */
+    @Override
+    protected boolean isAllowedToBeChildOf(LayoutNode futureParent) {
+        return true;
+    }
+
+    /**
+     * If this LayoutLeafs containedHashCode equals to hashDeletes, this function returns
+     * the parent of this LayoutLeaf with this LayoutLeaf deleted from it
+     */
     @Override
     public Layout delete(int hashToDelete) {
         if(containedHashCode == hashToDelete){
@@ -162,47 +188,38 @@ public class LayoutLeaf extends Layout {
 
 
     /**
-     * Makes a deepcopy of LayoutLeaf
-     * The references to this object and its contents will be removed
+     * Deletes this LayoutLeaf from its parent list of children, if it has a parent
+     */
+    @Override
+    protected void deleteLeftLeaf() {
+        if (super.parent != null) {
+            super.parent.delete(this);
+        }
+    }
+
+    /**
+     * Returns a copy of LayoutLeaf
+     * The references to this object will be removed
+     * The parent of this LayoutLeaf will not be brought over to the clone
      */
     @Override
     public LayoutLeaf clone() {
-            return new LayoutLeaf(containedHashCode);
+        return new LayoutLeaf(containedHashCode);
     }
 
-    @Override
-    public HashMap<Integer, Rectangle> getCoordsList(Rectangle uiCoordsScaled) {
-        HashMap<Integer, Rectangle> mapToReturn = new HashMap<Integer, Rectangle>();
-        mapToReturn.put(this.containedHashCode, uiCoordsScaled);
-        return mapToReturn;
-    }
 
     /**
-     * Makes the leftmost leaf active
-     * This is a leaf of the layout-structure, so it doesn't have any children, so this leaf should be madea active
+     * Returns true if this LayoutLeaf and the given Object are equal content-wise, false otherwise
+     * They are equal content-wise when the object is a LayoutLeaf and contains the same hashCode
      */
     @Override
-    protected int getLeftmostContainedHash() {
-        return containedHashCode;
+    public boolean equals(Object obj) {
+        if (obj instanceof LayoutLeaf leaf) {
+            return (this.containedHashCode == leaf.containedHashCode);
+        } else {
+            return false;
+        }
     }
 
-    /**
-     * Makes the rightmost leaf active
-     * This is a leaf of the layout-structure, so it doesn't have any children, so this leaf should be madea active
-     */
-    @Override
-    protected int getRightmostContainedHash() {
-        return containedHashCode;
-    }
-
-    /**
-     * Sets a new HashCode for the LayoutLeaf connected to the target HashCode
-     */
-    @Override
-    public void changeHash(int target, int newHashCode) {
-        if(this.containedHashCode == target)
-            this.containedHashCode = newHashCode;
-
-    }
 }
 
