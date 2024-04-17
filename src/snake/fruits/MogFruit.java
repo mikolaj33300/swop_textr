79f57061156bb185d0e91b7ee5aa2ee87e63b1f9
spykyvenator <spykyvenator@protonmail.com>
2024-04-17 18:54:43 +0200

    add: documentation for mogfruit

--- src/snake/fruits/MogFruit.java
+++ src/snake/fruits/MogFruit.java
@@ -6,6 +6,10 @@ public class MogFruit extends Fruit {
 
     private static final String CHARACTER = "M";
 
+    /**
+     * constructor for MogFruit
+     * @param p the position of the fruit
+     */
     public MogFruit(Pos p) {
         super(CHARACTER, 30, 6969, p);
     }
