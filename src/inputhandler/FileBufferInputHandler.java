7b2c55c97773d2e971ca5bc67b0e9c08faeaebae
spykyvenator <spykyvenator@protonmail.com>
2024-04-17 16:29:40 +0200

    add: doc for filebufferinput movedir userpop view debug

--- src/inputhandler/FileBufferInputHandler.java
+++ src/inputhandler/FileBufferInputHandler.java
@@ -22,6 +22,10 @@ public class FileBufferInputHandler extends InputHandlingElement {
 		return fb;
 	}
 
+  /**
+   * handle input
+   * @param b the input
+   */
 	public void input(byte b) {
 		switch(b) {
 			case 27:
@@ -38,13 +42,17 @@ public class FileBufferInputHandler extends InputHandlingElement {
 		}
 	}
 
+  /**
+   * @return if the buffer is dirty
+   */
 	@Override
 	public boolean isSafeToClose() {
 		return !fb.getDirty();
 	}
 
-	/*
+    /**
      * handles normal input
+     * @param b the input byte
      */
 	public void asciiInput(byte b) {
 		switch(b) {
@@ -83,8 +91,10 @@ public class FileBufferInputHandler extends InputHandlingElement {
 			break;
 		}
 	}
-	/*
+
+	/**
 	 * handles surrogate input
+   * @param b the input byte
 	 */
 	public void surrogateKeysInput(byte b) {
 		switch (b) {
@@ -107,38 +117,62 @@ public class FileBufferInputHandler extends InputHandlingElement {
 		}
 	}
 
+  /**
+   * handle right arrow input
+   */
 	@Override
 	public void handleArrowRight(){
 		fb.moveCursorRight();
 	}
 
+  /**
+   * handle left arrow input
+   */
 	@Override
 	public void handleArrowLeft(){
 		fb.moveCursorLeft();
 	}
 
+  /**
+   * handle down arrow input
+   */
 	@Override
 	public void handleArrowDown(){
 		fb.moveCursorDown();
 	}
 
+  /**
+   * handle up arrow input
+   */
 	@Override
 	public void handleArrowUp(){
 		fb.moveCursorUp();
 	}
 
+  /**
+   * enter a separator in the buffer
+   */
 	@Override
 	public void handleSeparator() throws IOException {
 		fb.enterSeparator();
 		fb.moveCursorRight();
 	}
 
+  /**
+   * close the filebuffer
+   * @return if the filebuffer was closed
+   */
 	@Override
 	public int close(){
 		return fb.close();
 	}
 
+  /**
+   * save the filebuffer
+   */
 	@Override
-	public void save() {this.fb.save(); }
+	public void save() {
+    this.fb.save(); 
+  }
 
 }
