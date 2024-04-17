d1c482a6ecf14d4cc93789b973b0b5df32b53085
mkbrk3 <mikeman55555@proton.me>
2024-04-17 15:44:07 +0200

    Fixed rendering and cursor drawing cfr assignment requirements

--- src/ui/FileBufferView.java
+++ src/ui/FileBufferView.java
@@ -33,7 +33,7 @@ public class FileBufferView extends View{
         int startX = coords.startX;
 
         //height-1 to make space for status bar, rounds to select the area from the nearest multiple of height-1
-        int renderStartingLineIndex = (containedFileBuffer.getInsertionPointLine() / (height - 1)) * (height - 1);
+        int renderStartingLineIndex = containedFileBuffer.getInsertionPointLine();
         //Renders either all the lines until the end, or the next height-2 lines
         for (int i = 0; i < Math.min(height - 1, containedFileBuffer.getLines().size() - renderStartingLineIndex); i++) {
             String lineString = new String(FileAnalyserUtil.toArray(containedFileBuffer.getLines().get(renderStartingLineIndex + i)));
@@ -117,7 +117,7 @@ public class FileBufferView extends View{
         int startX = coords.startX;
 
         int cursorXoffset = containedFileBuffer.getInsertionPointCol() % (width-1);
-        int cursorYoffset = containedFileBuffer.getInsertionPointLine() % (height-1);
+        int cursorYoffset = 0;//containedFileBuffer.getInsertionPointLine() % (height-1);
         termiosTerminalAdapter.moveCursor(1 + startY + cursorYoffset, 1 + startX + cursorXoffset);
     }
 }
