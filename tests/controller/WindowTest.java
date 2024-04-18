6827744088d26a2dd299a228a0db92ddf7c0cc30
spykyvenator <spykyvenator@protonmail.com>
2024-04-18 11:35:09 +0200

    remove: all testresources

--- tests/controller/WindowTest.java
+++ tests/controller/WindowTest.java
@@ -12,9 +12,16 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class WindowTest {
+    @TempDir
+    Path path1;
+
+    @BeforeEach
+    public void setVariables() throws IOException {
+        path1 = path1.resolve("test");
+    }
     @Test
     void testConstructor() throws IOException {
-        String path = "testresources/test.txt";
+        String path = path1.toString();
         BufferCursorContext bufferCursorContext = new BufferCursorContext(path, new byte[]{0x0d, 0x0a});
         FileBufferInputHandler handler = new FileBufferInputHandler(bufferCursorContext);
         VirtualTestingTermiosAdapter adapter = new VirtualTestingTermiosAdapter(20, 10, new ArrayList<>());
