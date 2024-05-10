81c515ec624688fa9878e8e9a72c2fe8b0feb44e
spykyvenator <spykyvenator@protonmail.com>
2024-05-10 14:25:02 +0200

    add: window link

--- src/controller/Window.java
+++ src/controller/Window.java
@@ -6,9 +6,31 @@ import ui.View;
 class Window {
     public final View view;
     public final InputHandlingElement handler;
+    private Window next;
 
     public Window(View v, InputHandlingElement h) {
         this.view = v;
         this.handler = h;
+    }
+
+    /*
+     * sander?
+     */
+    public Window(String path, byte[] lineSeparator){
+
+    }
+
+    public insertWindow(Window new) {
+      if (next == null) {
+        next = new;
+      } else {
+        new.insertWindow(new);
+      }
+      return;
+    }
+
+    public createFileBuffer(String path, byte[] lineSeparator, TermiosTerminalAdapter termiosTerminalAdapter) {
+      this.handler = new FileBufferInputHandler(checkPath, lineSeparatorArg);
+      this.view = new FileBufferView(openedFileHandler.getFileBufferContextTransparent(), termiosTerminalAdapter);
     }
 }
