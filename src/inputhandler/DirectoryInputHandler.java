57f66e107f0e1c2d5d471cdc63e988475b319c1c
spykyvenator <spykyvenator@protonmail.com>
2024-05-24 13:51:37 +0200

    more inputhandling doc

--- src/inputhandler/DirectoryInputHandler.java
+++ src/inputhandler/DirectoryInputHandler.java
@@ -8,58 +8,101 @@ import java.io.IOException;
 
 public class DirectoryInputHandler extends InputHandlingElement {
 
+  /**
+   * the directory this points towards
+   */
     private final Directory directory;
 
+    /**
+     * make a new instance of this with a Directory
+     */
     public DirectoryInputHandler(Directory dir) {
         this.directory = dir;
     }
 
+    /**
+     * make a new instance of this with a FileSystemEntry
+     */
     public DirectoryInputHandler(FileSystemEntry fe) {
         this.directory = new Directory(fe);
     }
 
+    /**
+     * it can be closed at any time 
+     */
     @Override
     public int forcedClose() {
         return 0;
     }
 
+    /**
+     * You can't save a Directory
+     */
     @Override
     public void save() {
         return;
     }
 
+    /**
+     * we have no input to be handled
+     * @return no rerender needed since nothing changed
+     */
     @Override
     public RenderIndicator input(byte b) throws IOException {
         return RenderIndicator.NONE;
     }
 
+    /**
+     * since there is nothing to be handled, we can close at any time
+     * @return true
+     */
     @Override
     public boolean isSafeToClose() {
         return true;
     }
 
+    /**
+     * we don't need to be able to move the cursor right
+     * @return no refresh
+     */
     @Override
     public RenderIndicator handleArrowRight() {
         return RenderIndicator.NONE;
     }
 
+    /**
+     * we don't need to be able to move the cursor left 
+     * @return no refresh
+     */
     @Override
     public RenderIndicator handleArrowLeft() {
         return RenderIndicator.NONE;
     }
 
+    /**
+     * we need to go one item down
+     * @return window refresh
+     */
     @Override
     public RenderIndicator handleArrowDown() {
         this.directory.increaseFocused();
         return RenderIndicator.WINDOW;
     }
 
+    /**
+     * we need to go one item up
+     * @return a window refresh
+     */
     @Override
     public RenderIndicator handleArrowUp() {
         this.directory.decreaseFocused();
         return RenderIndicator.WINDOW;
     }
 
+    /**
+     * when we get an enter we need to open the file under the cursor
+     * @return a window refresh
+     */
     @Override
     public RenderIndicator handleSeparator() throws IOException {
         if (this.directory.selectEntry() == null) {
@@ -68,10 +111,16 @@ public class DirectoryInputHandler extends InputHandlingElement {
         return RenderIndicator.WINDOW;
     }
 
+    /**
+     * @return the folder this points to
+     */
     public Directory getDirectory() {
         return this.directory;
     }
 
+    /**
+     * execute the visitors code on this
+     */
     public void accept(InputHandlerVisitor v) {
         v.visitDirectoryInputHandler(this);
     }
--- src/inputhandler/InputHandlerVisitor.java
+++ src/inputhandler/InputHandlerVisitor.java
@@ -1,7 +1,19 @@
 package inputhandler;
 
 public interface InputHandlerVisitor {
+  /**
+   * code to run on FileBufferInputHandler
+   * @param fh
+   */
     void visitFileInputHandler(FileBufferInputHandler fh);
+    /**
+     * Code to run on Snake
+     * @param sh
+     */
     void visitSnakeInputHandler(SnakeInputHandler sh);
+    /**
+     * Code to run on Directory
+     * @param dh
+     */
     void visitDirectoryInputHandler(DirectoryInputHandler dh);
 }
